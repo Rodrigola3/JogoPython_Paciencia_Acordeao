25846c2725b07bc50d09dab0f734d97e7d94d502
ykarosa <ykarosa@al.insper.edu.br>
2021-05-03 19:24:31 -0300

    organizando função de movimentos

--- lista_movimentos_possiveis.py
+++ lista_movimentos_possiveis.py
@@ -1,51 +1,27 @@
-def extrai_naipe(carta):
+def extrai_naipe(naipe):
+    lista2= []
+    for c in naipe:
+        lista2.append(c)
+    return lista2[-1]
 
-    naipe = carta[-1]
 
-    return naipe
-
-
-
-def extrai_valor(carta):
-
-    valor= carta[:-1]
-
+def extrai_valor(baralho):
+    valor= baralho[:-1] 
     return valor
 
-
-
 def lista_movimentos_possiveis(baralho, cont): 
-
-    novo= []
-
-   
-
+    novo= []  
     if cont == 0:
-
         return novo
 
-    if cont == 1 or cont == 2:
-
-
-
+    if cont == 1 or cont == 2:        
         if extrai_naipe(baralho[cont]) == extrai_naipe(baralho[cont - 1]) or  extrai_valor(baralho[cont]) == extrai_valor(baralho[cont - 1]):
-
             novo.append(1)
 
     if cont >= 3:
-
         if extrai_naipe(baralho[cont]) == extrai_naipe(baralho[cont - 3]) or  extrai_valor(baralho[cont]) == extrai_valor(baralho[cont - 3]):
-
             novo.append(3)
-
         if extrai_naipe(baralho[cont]) == extrai_naipe(baralho[cont - 1]) or  extrai_valor(baralho[cont]) == extrai_valor(baralho[cont - 1]):
-
             novo.append(1)
-
-        
-
-       
-
-            
 
     return novo
